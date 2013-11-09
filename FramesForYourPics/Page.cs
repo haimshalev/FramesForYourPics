26066c1bcc4b3013a265e66d9819c14997dfaced
haimshalev <haimshalev@gmail.com>
2013-11-10 01:48:17 +0200

    Added page classes  - a container called page list a page factory and a page object

---
+++ FramesForYourPics/Page.cs
@@ -0,0 +1,66 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace FramesForYourPics
+{
+    /// <summary>
+    /// Page object will hold 
+    /// </summary>
+    public class Page
+    {
+        #region Members
+
+        private readonly Image _pageBitmap;
+        private int _currentLine;
+        private int _currentColumn;
+        private int _numOfPhotos; 
+
+        #endregion
+
+        public Page()
+        {
+            //Create a bitmap which can hold 8 photos (4X2)
+            _pageBitmap = new Bitmap(Constants.ScaledWidth * Constants.NumberOfColumnsInAPage , Constants.ScaledHeight * Constants.NumberOfRowsInAPage);
+        }
+
+        /// <summary>
+        ///Save the page to the specified FilePath
+        /// </summary>
+        /// <param name="outputFilePath">the destination file path</param>
+        public void Save(string outputFilePath)
+        {
+            //Save the page to the specified FilePath
+            _pageBitmap.Save(outputFilePath, ImageFormat.Jpeg);
+        }
+
+        /// <summary>
+        /// Adds a photo to the current free place in the page
+        /// </summary>
+        /// <param name="photoPath">the path for the source photo</param>
+        public void AddPhoto(string photoPath)
+        {
+            //If we exceeded the number of photos allowed in a page
+            if (_numOfPhotos == Constants.NumberOfPhotosInAPage) return;
+
+            //Load the photo from the hard disk
+            using (var photo = Image.FromFile(photoPath))
+            {
+                //Draw the image on the bitmap
+                ImageManipulationUtils.DrawImageOnBitmap(_pageBitmap, photo, _currentColumn * Constants.ScaledWidth, _currentLine * Constants.ScaledHeight);
+
+                #region Update the class counters
+                
+                //Update the current column (0 or 1)
+                _currentColumn = (_currentColumn + 1) % 2;
+
+                //If the column is zero we need to change line 
+                if (_currentColumn == 0) _currentLine++;
+
+                //Increment the number of photos
+                _numOfPhotos++;
+ 
+                #endregion
+            }
+        }
+    }
+}
---
+++ FramesForYourPics/PageList.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FramesForYourPics
+{
+    /// <summary>
+    /// Contains a page list and initialize it
+    /// </summary>
+    public class PageList
+    {
+        private readonly string _inputFolder;
+        private readonly List<Page> _pageList = new List<Page>();
+
+        /// <summary>
+        /// Constructor - Create a page list from the photos in the input folder 
+        /// </summary>
+        /// <param name="inputFolder">the source folder path</param>
+        public PageList(string inputFolder)
+        {
+            //Set the input folder
+            _inputFolder = inputFolder;
+            
+            //While there are still files in the temp folder
+            while (Directory.GetFiles(_inputFolder).Length != 0)
+            {
+                //Create page from the files in the input folder and delete the handled files
+                _pageList.Add(PageFactory.CreatePage(_inputFolder));
+            }
+        }
+
+        /// <summary>
+        /// Save the inner page list to the hard drive
+        /// </summary>
+        /// <param name="outputFolder">the destination folder</param>
+        public void Save(string outputFolder = Constants.OutputFolder)
+        {
+            //Count the number of pages
+            var pageNum = 0;
+
+            //Save each page to the specified path
+            foreach (var page in _pageList)
+                page.Save(outputFolder + "Page" + (pageNum++) + Constants.JpegPhotoType);
+
+        }
+    }
+}
