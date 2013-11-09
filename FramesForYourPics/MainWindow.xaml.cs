cc13efc88817cfe8ec5a6559d8ded654e5c014ec
haimshalev <haimshalev@gmail.com>
2013-11-09 02:13:35 +0200

    Added a photo class which holds all the data which saved for every item template in the UI . Uses two way data binding to change the UI and the source.
    Updated the ui to hold a listbox which binded to a photolist object.
    
    Currently the main window functions including test code (will be deleted later)

--- FramesForYourPics/MainWindow.xaml.cs
+++ FramesForYourPics/MainWindow.xaml.cs
@@ -1,28 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace FramesForYourPics
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private const string TestPhoto = @"Resources\TestPhoto.jpg";
+        private const string MyFrame = @"Resources\MyFrame.png";
+        private const string OutputPhoto = @"Resources\OutputPhoto.jpeg";
+        private const string OutputPage = @"Resources\OutputPage.jpeg";
+
+        private readonly PhotoList _photoItems = new PhotoList();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Set the data binding 
+            lvPhotos.DataContext = _photoItems;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Image outputImage;
+
+            _photoItems.Add(new Photo(TestPhoto));
+
+            //Merge the two photos
+            using (Image photo = Image.FromFile(TestPhoto), mainFrame = Image.FromFile(MyFrame))
+            {
+                outputImage = ImageManipulationUtils.MergePhotoAndFrame(photo, mainFrame);
+
+                outputImage.Save(OutputPhoto, ImageFormat.Jpeg);
+            }
+
+            //Concatenate the two photos
+            using (Image photo1 = Image.FromFile(OutputPhoto), photo2 = Image.FromFile(OutputPhoto))
+            {
+                outputImage = ImageManipulationUtils.ConcatenateTwoImagesHorizontal(photo1, photo2);
+
+                outputImage.Save(OutputPage, ImageFormat.Jpeg);
+            }
+
+            //Concatenate the two lines
+
+            using (Image line1 = Image.FromFile(OutputPage), line2 = Image.FromFile(OutputPage))
+            {
+                outputImage = ImageManipulationUtils.ConcatenateTwoImagesVertical(line1, line2);
+            }
+
+            if (outputImage != null)
+                outputImage.Save(OutputPage, ImageFormat.Jpeg);
         }
     }
 }
+
---
+++ FramesForYourPics/Photo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
+using FramesForYourPics.Annotations;
+
+namespace FramesForYourPics
+{
+
+    /// <summary>
+    /// An observable lost of photo objects, used for binding to a list box
+    /// </summary>
+    public class PhotoList : ObservableCollection<Photo>{}
+
+    /// <summary>
+    /// Holds all the data which saved for every item template in the UI
+    /// Uses two way data binding to change the UI and the source
+    /// </summary>
+    public class Photo : INotifyPropertyChanged
+    {
+        public Photo(string picturePath)
+        {
+            // At first each picture need to be printed once 
+            NumberOfTimes = 1;
+
+            //Set the picture path
+            _picturePath = picturePath;
+
+            //Set the picture itself
+            Picture = new BitmapImage(new Uri(Path.GetFullPath(_picturePath)));
+        }
+
+        #region Members
+
+        private readonly string _picturePath;
+        private BitmapImage _picture;
+        private uint _numberOfTimes; 
+
+        #endregion
+
+        #region Properties
+
+        public BitmapImage Picture
+        {
+            get { return _picture; }
+            set { _picture = value; OnPropertyChanged(); }
+        }
+
+        public uint NumberOfTimes
+        {
+            get { return _numberOfTimes; }
+            set { _numberOfTimes = value; OnPropertyChanged(); }
+        } 
+
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
