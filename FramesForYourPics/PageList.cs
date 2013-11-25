819d1f1da7cda97e08b1118635ae2be27f6c05e0
haimshalev <haimshalev@gmail.com>
2013-11-25 19:31:31 +0200

    Add catch paragraph to the PageList Constructor- Catches all the cases when the input directory not found

--- FramesForYourPics/PageList.cs
+++ FramesForYourPics/PageList.cs
@@ -16,15 +16,25 @@ namespace FramesForYourPics
         /// <param name="outputFolder">the path to the folder which the pages will be saved in</param>
         public PageList(string inputFolder , string outputFolder = Constants.OutputFolder)
         {
-            //While there are still files in the temp folder
-            while (Directory.GetFiles(inputFolder).Length != 0)
+            try
             {
-                //Create page from the files in the input folder and delete the handled files
-                var p = PageFactory.CreatePage(inputFolder);
+                //While there are still files in the temp folder
+                while (Directory.GetFiles(inputFolder).Length != 0)
+                {
+                    //Create page from the files in the input folder and delete the handled files
+                    var p = PageFactory.CreatePage(inputFolder);
 
-                //Save the page to the hard drive
-                p.Save(outputFolder + "Page" + (_numOfPagesCreated++) + Constants.OutputFileType);
+                    //Save the page to the hard drive
+                    p.Save(outputFolder + "Page" + (_numOfPagesCreated++) + Constants.OutputFileType);
+                }
             }
+            catch (DirectoryNotFoundException e)
+            {
+                //If the directory is invalid create it
+                Directory.CreateDirectory(inputFolder);
+            }
+
+
         }
     }
 }
