e67569fb21bffcfe691da32c5f7d25e8a11c780e
haimshalev <haimshalev@gmail.com>
2013-11-27 21:23:10 +0200

    Updated the assemblt title to be עולמגנט

--- FramesForYourPics/Properties/AssemblyInfo.cs
+++ FramesForYourPics/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("עולם מגנט")]
+[assembly: AssemblyTitle("עולמגנט")]
 [assembly: AssemblyDescription("הכן דפי תמונות המוכנים להדפסת מגנטים")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Haim Shalelasvili")]
